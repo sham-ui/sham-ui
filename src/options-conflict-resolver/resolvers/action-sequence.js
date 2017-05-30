85c6d388ddb510ec554fb26bb48df1c5177a0e84
Eugeny Burnashov <shamcode@gmail.com>
2017-05-31 00:05:18 +0600

    Process static html for options-conflict-resolver

--- src/options-conflict-resolver/resolvers/action-sequence.js
+++ src/options-conflict-resolver/resolvers/action-sequence.js
@@ -4,7 +4,7 @@ export default class ActionSequenceAndHandlerResolver extends OptionsConflictRes
     predicate( widget, options ) {
         return widget.handlerProps &&
             'bindEvents' === options.actionSequence[ 0 ] &&
-            undefined !== widget.html
+            ( undefined !== widget.html || undefined !== widget.constructor.html )
     }
 
     resolve( options ) {
--- src/options-conflict-resolver/resolvers/bind-once.js
+++ src/options-conflict-resolver/resolvers/bind-once.js
@@ -4,7 +4,7 @@ export default class BindOnceAndHandlerResolver extends OptionsConflictResolver 
     predicate( widget, options ) {
         return widget.handlerProps &&
         'render' === options.actionSequence[ 0 ] &&
-        undefined !== widget.html &&
+        ( undefined !== widget.html || undefined !== widget.constructor.html ) &&
         options.bindOnce
     }
 
