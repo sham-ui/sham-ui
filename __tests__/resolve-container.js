4701c0391710c5fd37bf555fb88532719517a6b7
Eugeny Burnashov <shamcode@gmail.com>
2018-09-24 20:09:41 +0700

    Migrate resolve container to jest

---
+++ __tests__/resolve-container.js
@@ -0,0 +1,34 @@
+import { DI, Widget } from '../src/shamUI';
+import { renderWidget, renderApp } from './helpers';
+
+
+it( 'bindEvents', async() => {
+    expect.assertions( 1 );
+    class Dummy extends Widget {
+        bindEvents() {
+            expect( this.container ).toBeInstanceOf( Element );
+        }
+    }
+    await renderWidget( Dummy );
+} );
+
+it( 'render', async() => {
+    expect.assertions( 1 );
+    class Dummy extends Widget {
+        render() {
+            expect( this.container ).toBeInstanceOf( Element );
+        }
+    }
+    await renderWidget( Dummy );
+} );
+
+it( 'empty widget', async() => {
+    expect.assertions( 1 );
+    class Dummy extends Widget {}
+    let widget;
+    DI.bind( 'widget-binder', () => {
+        widget = new Dummy( 'body', 'dummy' );
+    } );
+    await renderApp();
+    expect( widget.container ).toBeInstanceOf( Element );
+} );
